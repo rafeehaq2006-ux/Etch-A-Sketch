467041f7a94f2ed40791d225c5b93b021b5f0dbe
Rafee <rafeehaq2006@gmail.com>
2026-07-11 13:00:27 +0100

    Created 16x16 grid with javascript

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const Outer = document.querySelector(".Outer");
+for (let i =0; i<16; i++){
+    div = document.createElement("div");
+    div.classList.add("row");
+    div.style.display = "flex";
+    for (let k = 0; k <16; k++){
+        innerdiv = document.createElement("div");
+        innerdiv.classList.add("box");
+        innerdiv.style.border = "2px solid black";
+        innerdiv.style.height = "40px";
+        innerdiv.style.width="40px";
+        div.appendChild(innerdiv);
+    }
+    Outer.appendChild(div);
+}
