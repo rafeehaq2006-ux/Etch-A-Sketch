d6405c521f197b6617c7980ad838f99eb07f5fe7
Rafee <rafeehaq2006@gmail.com>
2026-07-12 15:55:59 +0100

    added a clear grid ability

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 const Outer = document.querySelector(".Outer");
-const button = document.querySelector("button");
+const sizebutton = document.querySelector("#size");
+const clearbutton = document.querySelector('#clear');
 let boxes = [];
+
 function removeBoxes(){
     console.log(boxes);
     boxes.forEach((box) => {
@@ -39,7 +41,15 @@ function attachHoverListeners(){
     });
 }
 
-button.addEventListener("click", () => {
+clearbutton.addEventListener("click", () => {
+    if (boxes.length > 0){
+        boxes.forEach((box) => {
+            box.style.backgroundColor = "white";
+        });
+    };
+});
+
+sizebutton.addEventListener("click", () => {
     let size = parseInt(prompt("Enter your grid size"));
     defineGrid(size);
 })
